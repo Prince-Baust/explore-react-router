fb2259e6f77aa1c9bba54f7b36f494ae76b7d249
Maruf Al Hossain <pmaruf42@gmail.com>
2021-10-23 22:59:28 +0600

    load data from dynamic route

--- src/components/FriendDetails/FriendDetails.js
+++ src/components/FriendDetails/FriendDetails.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useParams} from "react-router-dom";
 
 const FriendDetails = () => {
+    const [friend, setFriend] = useState({});
+    const {friendID} = useParams();
+    const url = `https://jsonplaceholder.typicode.com/users/${friendID}`;
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setFriend(data));
+    }, []);
+
+    const {name, phone, email, website, company} = friend;
+
     return (
         <div>
-            <h3>Coming Soon...</h3>
+            <h2>{name}</h2>
+            <h3>{phone}</h3>
+            <h4>{email}</h4>
+            <h5>{website}</h5>
+            <h5>{company?.name}</h5>
         </div>
     );
 };
