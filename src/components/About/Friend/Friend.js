3714b4669aea5bb82a60372e4656d2b0f092a292
Maruf Al Hossain <pmaruf42@gmail.com>
2021-10-23 19:23:46 +0600

    friend fetch and display

---
+++ src/components/About/Friend/Friend.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Friend = ({friend}) => {
+    const {name, phone, email, website, address} = friend;
+    return (
+        <div style={{border: "3px solid blue",
+            borderRadius: "10px",
+            padding: "5px",
+            margin: "10px"
+        }}>
+            <h2>{name}</h2>
+            <h5>{phone}</h5>
+            <h5>{email}</h5>
+            <h5>{website}</h5>
+            <p><small>{address.city}</small></p>
+        </div>
+    );
+};
+
+export default Friend;
--- src/components/Firends/Friends.js
+++ src/components/Firends/Friends.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Friend from "../About/Friend/Friend";
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setFriends(data));
+    }, []);
+
     return (
         <div>
-            <h2>This is friends</h2>
+            <h2>I have {friends.length} friends</h2>
+            <div>
+                {
+                    friends.map(friend => <Friend key={friend.id} friend={friend}/>)
+                }
+            </div>
         </div>
     );
 };
